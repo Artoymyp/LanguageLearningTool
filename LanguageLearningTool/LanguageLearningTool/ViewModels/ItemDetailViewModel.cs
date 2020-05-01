4b1b21a4ae650519ed7167d2356a1eea2a8f47fe
Artoymyp <artoymyp@gmail.com>
2020-05-01 23:01:24 +0300

    Initial commit. Standard cross-platform Xamarin solution with default master-detail form.

---
+++ LanguageLearningTool/LanguageLearningTool/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LanguageLearningTool.Models;
+
+namespace LanguageLearningTool.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
