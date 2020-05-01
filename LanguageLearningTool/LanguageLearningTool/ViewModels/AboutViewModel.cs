5c7c3bfdddddb3483e793a1b1e42f77994bfcc41
Artoymyp <artoymyp@gmail.com>
2020-05-02 00:04:21 +0300

    Implemented simple cross-platform audio support using Xam.Plugin.SimpleAudioPlayer.

--- LanguageLearningTool/LanguageLearningTool/ViewModels/AboutViewModel.cs
+++ LanguageLearningTool/LanguageLearningTool/ViewModels/AboutViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Input;
 
 using Xamarin.Forms;
@@ -12,8 +14,19 @@ namespace LanguageLearningTool.ViewModels
 			Title = "About";
 
 			OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+			PlayTestCommand = new Command(()=>
+			{
+				var assembly = typeof(App).GetTypeInfo().Assembly;
+				var infos = assembly.GetManifestResourceNames();
+				Stream audioStream = assembly.GetManifestResourceStream("LanguageLearningTool.Resources." + "test.mp3");
+
+				var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+				player.Load(audioStream);
+				player.Play();
+			});
 		}
 
 		public ICommand OpenWebCommand { get; }
+		public ICommand PlayTestCommand { get; }
 	}
 }
